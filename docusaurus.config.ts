ab2bff1b90df0d24e0c9e86f4306ade9b55587bf
hamideh2050 <165998425+hamideh2050@users.noreply.github.com>
2024-11-22 12:38:17 +0100

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'TEACH 4',
+  title: 'Hello',
   tagline: 'My New Web',
   favicon: 'img/favicon.ico',
 
